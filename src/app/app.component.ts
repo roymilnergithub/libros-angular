6bb8b5371dd233b61313f22106e665d5523ca450
MacBook Air <macbookair@MacBooks-MacBook-Air-2.local>
2019-06-13 22:55:24 -0500

    correción menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit() {
+    $(document).ready(function(){
+      $.getScript("assets/librerias/animsition/js/animsition.min.js");
+      $.getScript("assets/js/main.js");
+      // $.getScript("assets/librerias/jquery/jquery-3.2.1.min.js");
+      // $.getScript("assets/librerias/bootstrap/js/popper.js");
+      // $.getScript("assets/librerias/bootstrap/js/bootstrap.min.js");
+      // $.getScript("assets/librerias/select2/select2.min.js");
+      // $.getScript("assets/librerias/daterangepicker/moment.min.js");
+      // $.getScript("assets/librerias/daterangepicker/daterangepicker.js");
+      // $.getScript("assets/librerias/slick/slick.min.js");
+      // $.getScript("assets/js/slick-custom.js");
+      // $.getScript("assets/librerias/parallax100/parallax100.js");
+      // $.getScript("assets/librerias/MagnificPopup/jquery.magnific-popup.min.js");
+      // $.getScript("assets/librerias/isotope/isotope.pkgd.min.js");
+      // $.getScript("assets/librerias/sweetalert/sweetalert.min.js");
+      // $.getScript("assets/librerias/perfect-scrollbar/perfect-scrollbar.min.js");
+    });
+
+  }
+
 }
--- src/app/pages/novedades/novedades.component.ts
+++ src/app/pages/novedades/novedades.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 // import { Location } from '@angular/common';
 // import { Util } from './../../common/util/util';
 import { Global } from './../../common/global/global';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-novedades',
@@ -26,28 +25,6 @@ export class NovedadesComponent implements OnInit {
 
   ngOnInit() {
     this.loadList();
-
-    $(document).ready(function(){
-      // $.getScript("assets/librerias/animsition/js/animsition.min.js");
-
-// $.getScript("assets/librerias/jquery/jquery-3.2.1.min.js");
-$.getScript("assets/librerias/animsition/js/animsition.min.js");
-// $.getScript("assets/librerias/bootstrap/js/popper.js");
-// $.getScript("assets/librerias/bootstrap/js/bootstrap.min.js");
-// $.getScript("assets/librerias/select2/select2.min.js");
-// $.getScript("assets/librerias/daterangepicker/moment.min.js");
-// $.getScript("assets/librerias/daterangepicker/daterangepicker.js");
-// $.getScript("assets/librerias/slick/slick.min.js");
-// $.getScript("assets/js/slick-custom.js");
-// $.getScript("assets/librerias/parallax100/parallax100.js");
-// $.getScript("assets/librerias/MagnificPopup/jquery.magnific-popup.min.js");
-// $.getScript("assets/librerias/isotope/isotope.pkgd.min.js");
-// $.getScript("assets/librerias/sweetalert/sweetalert.min.js");
-// $.getScript("assets/librerias/perfect-scrollbar/perfect-scrollbar.min.js");
-$.getScript("assets/js/main.js");
-
-    });
-
   }
 
   loadList() {
